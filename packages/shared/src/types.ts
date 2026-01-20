0e248326ca7ebfe2693f013ca42a6f289b6af953
optcond <optimalcondition@tutanota.com>
2026-01-20 20:33:28 +0300

    usdt ca added

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -98,6 +98,10 @@ export const EthTokens: Record<string, { address: string; decimals: number }> =
       address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
       decimals: 6,
     },
+    USDT: {
+      address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      decimals: 6,
+    },
   };
 
 /*
